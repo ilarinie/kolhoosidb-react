2352d697732d8fc9c366ffb54ee90a3d171e2a5a
Ilari Nieminen <ext_ilari.nieminen@samlink.fi>
2017-08-08 15:17:22 +0300

    Added base page for adding users

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -11,6 +11,7 @@ import { Communelist } from './communelist/communelist';
 import { CommuneSelectedRoute } from '../../App';
 import DevTools from 'mobx-react-devtools';
 import KolhoosiSnackBar from '../util/kolhoosi-snackbar';
+import { AddUserComponent } from './add-user';
 
 @inject('appState')
 @observer
@@ -25,6 +26,7 @@ export class Dashboard extends React.Component<{ appState: AppState }, {}> {
                     <Switch>
                         <CommuneSelectedRoute exact={true} path="/" component={TasksComponent} />
                         <CommuneSelectedRoute path="/purchases" component={PurchasesComponent} />
+                        <CommuneSelectedRoute path="/adduser" component={AddUserComponent} />
                         <Route path="/communelist" component={Communelist} />
                     </Switch>
                 </div>
