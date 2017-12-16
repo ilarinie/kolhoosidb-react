da17a8363d3a1341fefc6f8219966e050213531f
Ilari Nieminen <ilari.nieminen@outlook.com>
2017-12-17 00:21:22 +0200

    Added environment specific api-urls.

---
+++ src/environment.tsx
@@ -0,0 +1,32 @@
+
+const PROD_API_URL = 'https://kolhoosidb-api.ilarinieminen.me/';
+const INTEGRATION_API_URL = 'https://i-kolhoosidb-api.ilarinieminen.me/';
+const DEVELOPMENT_API_URL = 'http://localhost:3001/';
+
+export class Env {
+    apiUrl: string;
+    constructor(apiUrl: string) {
+        this.apiUrl = apiUrl;
+    }
+}
+
+export const getEnv = (): Env => {
+    if (process.env.REACT_APP_ENV === 'integration') {
+        return getIntegrationEnv();
+    }
+    if (process.env.NODE_ENV === 'production') {
+        return getProductionEnv();
+    } else {
+        return getDevelopmentEnv();
+    }
+}
+
+const getProductionEnv = (): Env => {
+    return new Env(PROD_API_URL);
+};
+const getDevelopmentEnv = (): Env => {
+    return new Env(DEVELOPMENT_API_URL);
+};
+const getIntegrationEnv = (): Env => {
+    return new Env(INTEGRATION_API_URL);
+};
