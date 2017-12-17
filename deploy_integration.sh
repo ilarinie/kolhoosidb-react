0d289c46fb010cb037844882c9a38e2b95ebb98c
ilarinie <ilari.nieminen@outlook.com>
2017-12-17 11:17:39 +0200

    Travis integr deployment.

---
+++ deploy_integration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+rm ~/.ssh/known_hosts
+
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -t $USER@$IP<<EOF
+    $COMMAND
+EOF
